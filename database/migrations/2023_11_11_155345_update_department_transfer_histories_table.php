46382dfce5289ac06122366ef77e7c09cf2bda3e
Abu Oubaida <abuoubaida36@gmail.com>
2023-11-12 09:44:07 +0600

    user excel data upload done and department change with dynamic id

---
+++ database/migrations/2023_11_11_155345_update_department_transfer_histories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('department_transfer_histories', function (Blueprint $table) {
+            $table->string('old_employee_id')->nullable()->after('id');
+            $table->string('new_employee_id')->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
