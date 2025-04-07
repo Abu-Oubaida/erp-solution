ac05c7760559265a8fd0fa595d1d5bb8c16c6ae2
unknown <saiful250321016@credencegroupbd.com>
2025-04-07 16:41:28 +0600

    Data Archive Dashboard

--- resources/views/back-end/archive/dashboard.blade.php
+++ resources/views/back-end/archive/dashboard.blade.php
@@ -13,7 +13,7 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <table class="table table-bordered">
+                                <table class="table table-bordered mt-4">
                                     <tr>
                                         <th>Module Name</th>
                                         <th>:</th>
@@ -56,9 +56,9 @@
                                     </tr>
                                 </table>
                             </div>
-                            <div class="col-2">
+                            <div class="col">
                                 <div class="align-items-center">
-                                    <div id="chart-container" style="width: 300px; float:right">
+                                    <div id="chart-container" style="width: 355px; float:right">
                                         <canvas id="levelChart"
                                             style="width: 100% !important;height: 100% !important;"></canvas>
 
@@ -66,12 +66,15 @@
                                             <div class="progress-bar bg-danger" role="progressbar"
                                                 style="width: {!! $totalUsed !!}%"
                                                 aria-valuenow="{!! $totalUsed !!}" aria-valuemin="0"
-                                                aria-valuemax="{{ $diskTotal }}" title="Total Used ({!! $totalUsed !!}) GB">
+                                                aria-valuemax="{{ $diskTotal }}"
+                                                title="Total Used ({!! $totalUsed !!}) GB">
                                                 Used ( {!! $totalUsed !!} GB )</div>
                                             <div class="progress-bar bg-success" role="progressbar"
                                                 style="width: {!! $diskFree !!}%"
                                                 aria-valuenow="{!! $diskFree !!}" aria-valuemin="0"
-                                                aria-valuemax="{{ $diskTotal }}" title="Free Spase ({!! $diskFree !!}) GB">Free ( {!! $diskFree !!} GB )
+                                                aria-valuemax="{{ $diskTotal }}"
+                                                title="Free Spase ({!! $diskFree !!}) GB">Free (
+                                                {!! $diskFree !!} GB )
                                             </div>
                                         </div>
                                         <p class="text-center"><strong>Total Storage:</strong> {{ $diskTotal }} GB</p>
@@ -85,24 +88,24 @@
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">
-                        <h3><i class="fa-solid fa-hard-drive"></i> Storage Information Details</h3>
+                        <h3><i class="fa-solid fa-chart-column"></i> Previous Week's Uploaded Document</h3>
                     </div>
                     <div class="card-body">
                         <div class="row">
-
+                            <canvas id="documentTypeChart" width="600" class="mt-4"></canvas>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row mt-3">
-            @if(isset($dataTypes) && count($dataTypes))
+            @if (isset($dataTypes) && count($dataTypes))
                 @php
-                    $colors = ['info', 'success', 'primary', 'warning','secondary'];
+                    $colors = ['info', 'success', 'primary', 'warning', 'secondary'];
                     $prevColor = null;
                     $prevPrevColor = null;
                 @endphp
-                @foreach($dataTypes as $dataType)
+                @foreach ($dataTypes as $dataType)
                     @php
                         // Filter available colors
                         $availableColors = array_filter($colors, function ($color) use ($prevColor, $prevPrevColor) {
@@ -116,34 +119,25 @@
                         $prevPrevColor = $prevColor;
                         $prevColor = $chosenColor;
                     @endphp
-                    <div class="col">
+                    <div class="col-3">
                         <div class="card bg-{{ $chosenColor }} text-white mb-4">
                             <div class="card-header">
-                                <h4 class="text-capitalize"><i class="fas fa-file"></i> {!! $dataType['voucher_type_title'] !!}</h4>
+                                <h4 class="text-capitalize"><i class="fas fa-file-lines"></i> {!! $dataType['voucher_type_title'] !!}</h4>
                             </div>
                             <div class="card-body text-capitalize">
                                 <div class="row">
                                     <div class="col text-start">
                                         <h2 class="text-chl">{!! $dataType['archive_document_infos_count'] !!}</h2>
-                                        <a class="small text-white stretched-link text-decoration-none" href="#">Reference <i class="fas fa-angle-right"></i></a>
+                                        <a class="small text-white stretched-link text-decoration-none"
+                                            href="#">Reference <i class="fas fa-angle-right"></i></a>
                                     </div>
                                     <div class="col text-end">
                                         <h2 class="text-chl">{!! $dataType['archive_documents_count'] !!}</h2>
-                                        <a class="small text-white stretched-link text-decoration-none" href="#">Documents <i class="fas fa-angle-right"></i></a>
+                                        <a class="small text-white stretched-link text-decoration-none"
+                                            href="#">Documents <i class="fas fa-angle-right"></i></a>
                                     </div>
                                 </div>
-{{--                                <ul>--}}
-{{--                                    <li>Unique Archive Reference <h5 class="display-inline float-end"></h5></li>--}}
-{{--                                    <li>Data type wise document count <h5 class="display-inline float-end">{!! $dataType['archive_documents_count'] !!}</h5></li>--}}
-{{--                                </ul>--}}
                             </div>
-{{--                            <div class="card-footer d-flex align-items-center justify-content-between">--}}
-{{--                                <a class="small text-white stretched-link" href="#">View--}}
-{{--                                    Details</a>--}}
-{{--                                <div class="small text-white">--}}
-{{--                                    <i class="fas fa-angle-right"></i>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
                         </div>
                     </div>
                 @endforeach
@@ -153,15 +147,16 @@
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
     <script>
         const ctx = document.getElementById('levelChart').getContext('2d');
         const levelChart = new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: ["Free", 'Archive', 'Other',],
+                labels: ["Free", 'Archive', 'Other', ],
                 datasets: [{
                     label: 'Disk Usage',
-                    data: [{!! @$diskFree !!}, {!! @$archiveUsed !!}, {!! @$otherUsed !!},],
+                    data: [{!! @$diskFree !!}, {!! @$archiveUsed !!}, {!! @$otherUsed !!}, ],
                     backgroundColor: [
                         '#198754', // Free - Blue
                         '#da3545', // Archive Used
@@ -183,6 +178,75 @@
                     }
                 }
             }
+        });
+
+
+        const barCtx = document.getElementById('documentTypeChart').getContext('2d');
+
+        const documentTypeChart = new Chart(barCtx, {
+            type: 'bar',
+            data: {
+                labels: {!! json_encode($labels) !!},
+                datasets: [{
+                    label: 'Documents Uploaded per Day',
+                    data: {!! json_encode($documentCountsPerDay) !!},
+                    backgroundColor: ['rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ], // Light gray color for the bar
+                    borderColor: ['rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderWidth: 1,
+                    barPercentage: 0.5, //Decrease width of bars, 50% of the available space for each bar
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false // Hides the legend
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                return ` ${context.parsed.y} documents`; // Custom tooltip text
+                            }
+                        }
+                    },
+                    datalabels: {
+                        anchor: 'end',
+                        align: 'end',
+                        color: '#d3d3d3', // Light gray color for data labels
+                        font: {
+                            weight: 'bold'
+                        },
+                        formatter: function(value) {
+                            return value; // Simply displays the value
+                        }
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        min: 0,
+                        max: {!! json_encode($totalDocumentCount) !!}, // Y-axis range from 0 to max
+                        ticks: {
+                            stepSize: 5 // Increase by 10, i.e., 0, 10, 20
+                        }
+                    }
+                }
+            },
+            plugins: [ChartDataLabels] // Enables the ChartDataLabels plugin
         });
     </script>
 @stop
