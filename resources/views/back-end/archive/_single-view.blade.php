d115beb0085c848d12b7c18b4c18591406d21137
Abu Oubaida <abuoubaida36@gmail.com>
2025-04-27 17:13:30 +0600

    archive list some change

--- resources/views/back-end/archive/_single-view.blade.php
+++ resources/views/back-end/archive/_single-view.blade.php
@@ -7,11 +7,20 @@
     @endphp
     <div class="row">
         <div class="col-md-12 mb-2">
+            @php
+                if(\Illuminate\Support\Facades\Request::get('ocr'))
+                {
+                    $ocr = 1;
+                }
+                else{
+                    $ocr = null;
+                }
+            @endphp
             @if($previous_document_id)
-                <a href="{!! route('view.archive.document',['vID'=>\Illuminate\Support\Facades\Crypt::encryptString($previous_document_id),'ref'=>$ref_id]) !!}" class="btn btn-sm btn-outline-danger float-left"><i class="fas fa-angle-left"></i> Previous <i class="fas fa-file-lines"></i></a>
+                <a href="{!! route('view.archive.document',['vID'=>\Illuminate\Support\Facades\Crypt::encryptString($previous_document_id),'ref'=>$ref_id,'ocr'=>$ocr]) !!}" class="btn btn-sm btn-outline-danger float-left"><i class="fas fa-angle-left"></i> Previous <i class="fas fa-file-lines"></i></a>
             @endif
             @if($next_document_id)
-                <a href="{!! route('view.archive.document',['vID'=>\Illuminate\Support\Facades\Crypt::encryptString($next_document_id), 'ref'=>$ref_id]) !!}" class="btn btn-sm btn-outline-primary float-end"> <i class="fas fa-file-lines"></i> Next <i class="fas fa-angle-right"></i></a>
+                <a href="{!! route('view.archive.document',['vID'=>\Illuminate\Support\Facades\Crypt::encryptString($next_document_id), 'ref'=>$ref_id,'ocr'=>$ocr]) !!}" class="btn btn-sm btn-outline-primary float-end"> <i class="fas fa-file-lines"></i> Next <i class="fas fa-angle-right"></i></a>
             @endif
         </div>
         <div id="" class="">
