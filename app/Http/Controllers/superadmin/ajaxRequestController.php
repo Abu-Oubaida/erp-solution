48f09a9e8b4cf4e36a9b7655188538829f44a74a
Abu Oubaida <abuoubaida36@gmail.com>
2025-05-13 14:27:13 +0600

    Permission logic change

--- app/Http/Controllers/superadmin/ajaxRequestController.php
+++ app/Http/Controllers/superadmin/ajaxRequestController.php
@@ -175,7 +175,7 @@ class ajaxRequestController extends Controller
                     ]);
                 }
                 $companyWiseParentPermission = CompanyModulePermission::select('module_parent_id')->where('company_id',$cid)->distinct()->get();
-                $permissionParents = Permission::whereIn('id',$companyWiseParentPermission)->where('parent_id',null)->get();
+                $permissionParents = Permission::whereIn('id',$companyWiseParentPermission)->whereNot('is_parent',null)->get();
                 return response()->json([
                     'status' => 'success',
                     'message' => 'Permission successfully changed',
